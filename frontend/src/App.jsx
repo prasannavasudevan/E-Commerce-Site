3c65a2c7a54a460e4e8c5fac1c957a182c5ec979
Prasanna <v.prasanna816@gmail.com>
2025-05-17 20:29:32 +0530

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route} from "react-router-dom"
 import Home from './pages/Home'
-import Collection from './pages/collection'
+import Collection from './pages/Collection'
 import About from "./pages/About"
 import Product from "./pages/Product"
 import Login from './pages/Login'
